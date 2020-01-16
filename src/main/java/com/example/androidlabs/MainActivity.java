0cd43424eb13e0db2068c34e0eaa2804960349e3
MinLi <li000611@algonquinlive.com>
2020-01-16 13:24:46 -0500

    add more words

--- src/main/java/com/example/androidlabs/MainActivity.java
+++ src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
     }
-}
+/*
+    final CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+    if(checkBox.ischecked()){
+        checkBox.setChecked(false);
+    }
+
+
+    Button button=(Button)findViewById(R.id.checkBox);
+         button.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v){
+                 StringBuffer result = new StringBuffer();
+                 result.append("Here is more information").append(button.isSelected());
+                 Toast.makeText(MainActivity.this, result.toString(),
+                    Toast.LENGTH_LONG.show();
+
+        }
+
+    });
+
+*/
+    }
