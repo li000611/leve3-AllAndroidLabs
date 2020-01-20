1f0d0a8c12f952d07ec02d6876bcc37cac26048e
MinLi <li000611@algonquinlive.com>
2020-01-20 10:09:55 -0500

    add a click listener to the "Check this out"button. add a check changed listener to the Checkbox and Switch to show a Snackbar which says "The switch is now."

--- src/main/java/com/example/androidlabs/MainActivity.java
+++ src/main/java/com/example/androidlabs/MainActivity.java
@@ -25,40 +25,24 @@ import static com.example.androidlabs.R.id.editText;
 public class MainActivity<compoundButton> extends AppCompatActivity {
 
     private CoordinatorLayout coordinatorLayout;
-    Button mySecondButton = null;
+    CheckBox cb=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
-/*
-       // Button mySecondButton = null;
+        setContentView(R.layout.activity_main_relative);
 
-      //  mySecondButton = (Button) findViewById(R.id.checkBox);
+        cb = (CheckBox) findViewById(R.id.checkBox);
+        EditText theEdit = findViewById(R.id.editText);
+        Switch sb = findViewById(R.id.switch2);
 
-      CheckBox cb = findViewById(R.id.checkBox);
-        mySecondButton.setOnClickListener(new View.OnClickListener() {
+        cb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String toastMessage = MainActivity.this.getResources().getString(R.string.toast_message);
                 Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
             }
         });
-    }
-*/
-
-
-  /*The second way to add a click listener
-
-       public void myFirstMethod(View v){
-        Toast.makeText(getApplicationContext(),"Here is more information",Toast.LENGTH_SHORT).show();
-       }
-*/
-
-
-        EditText theEdit = findViewById(R.id.editText);
-        Switch sb = findViewById(R.id.switch2);
-
 
         sb.setOnCheckedChangeListener((compoundButton, b) -> {
             Snackbar.make(theEdit, "Switch is on " + b, Snackbar.LENGTH_LONG)
@@ -66,20 +50,18 @@ public class MainActivity<compoundButton> extends AppCompatActivity {
                     .show();
         });
 
-        CheckBox cb = findViewById(R.id.checkBox);
 
         cb.setOnCheckedChangeListener((compoundButton, b) -> {
             Snackbar.make(theEdit, "Checkbox is checked " + b, Snackbar.LENGTH_LONG)
                     .setAction("Undo", click -> compoundButton.setChecked(b))
                     .show();
         });
-
     }
 
     public void BasicToast(View view ){
         Context context = getApplicationContext();
         CharSequence text = getResources().getString(R.string.toast_message);
-        int duration = Toast.LENGTH_SHORT;
+        int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context,text,duration);
         toast.show();
     }
