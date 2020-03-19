5e3ce585ae33998ba5d5f1006e506dfec9b5a1d8
MinLi <li000611@algonquinlive.com>
2020-03-19 13:52:38 -0400

    added chat.png

--- src/main/java/com/example/androidlabs/TestToolbar.java
+++ src/main/java/com/example/androidlabs/TestToolbar.java
@@ -35,7 +35,7 @@ public class TestToolbar extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
 
-        MenuItem searchItem = menu.findItem(R.id.);
+        MenuItem searchItem = menu.findItem(R.id.search_item);
         SearchView sView = (SearchView)searchItem.getActionView();
         sView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -49,8 +49,41 @@ public class TestToolbar extends AppCompatActivity {
         return true;
     }
 
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item2:
+                alertExample();
+            case R.id.item3:
+                 Snackbar sb = Snackbar.make(myToolbar, "Go Back?", Snackbar.LENGTH_LONG)
+                         .setAction("Finish", e-> finish());
+                 sb.show();
+                 break;
+            case R.id.item4:
+                Toast.makeText(this,"You clicked on the overflow menu",Toast.LENGTH_LONG).show();
+            }
+            return true;
+    }
 
+    private void alertExample() {
+        View middle = getLayoutInflater().inflate(R.layout.item2_dialogbox, null);
+        EditText et = (EditText)middle.findViewById(R.id.textView13);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("The Message")
+                .setPositiveButton("Positive",new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        message=et.getText().toString();
+                    }
+                })
+                .setNegativeButton("Negative", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id){
+                        }
+                }).setView(middle);
+        builder.create().show();
+    }
 
 
 }
